d10af89231d4f4a0369a3d28587d238be0241e8a
vinay8698 <vinodkumarkanike00@gmail.com>
2026-02-23 18:36:39 +0530

    1st change 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1>Vinodkumar Kanike</h1>
+      <h1>Vinodkumar muniraj Kanike</h1>
     </div>
   );
 }
